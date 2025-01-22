bd37d5ea02bd4faf64f3b7c4df21433be05ef278
Pr4sus <pslguipo22@src.pshs.edu.ph>
2025-01-22 11:10:03 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-<script>
-	const PI = 3.14;
-	var radius;
-	let circumference = 2*PI*radius;
-	let surfaceArea = 4*PI*(radius)^^2;
-	let volume = (4/3)*PI*(radius)^^3
+const PI = 3.14;
+var radius;
+let circumference = 2*PI*radius;
+let surfaceArea = 4*PI*(radius)^^2;
+let volume = (4/3)*PI*(radius)^^3;
 	
-	document.write('The radius of the sphere is ' + radius);
-	document.write('The circumference is ' + circumference);
-	document.write('The surface area is ' + surfaceArea);
-	document.write('The volume is ' + volume);
+document.write('The radius of the sphere is ' + radius);
+document.write('The circumference is ' + circumference);
+document.write('The surface area is ' + surfaceArea);
+document.write('The volume is ' + volume);
 	
-	var radius = 6378000;
+var radius = 6378000;
 	
-</script>
+
 	
 	
